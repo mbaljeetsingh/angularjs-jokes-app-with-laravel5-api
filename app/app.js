3780c0fa0f6c9a380309fb93b5adf6a6181f68c1
mbaljeetsingh <bsinghs.007@gmail.com>
2015-10-27 14:33:56 +0530

    Added ui-router to the angular seed project

--- app/app.js
+++ app/app.js
@@ -2,11 +2,15 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
+  // 'ngRoute',
+  'ui.router',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+// config(['$routeProvider', function($routeProvider) {
+//   $routeProvider.otherwise({redirectTo: '/view1'});
+// }]);
+config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/view1');
 }]);
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,12 +1,14 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.view2', ['ui.router'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('view2', {
+    url: '/view2',
+    templateUrl: "view2/view2.html",
     controller: 'View2Ctrl'
-  });
+  })
 }])
 
 .controller('View2Ctrl', [function() {
